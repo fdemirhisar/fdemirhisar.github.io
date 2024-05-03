bbbd0ed871f5c12db2649377d0424b3758190e27
Filiz <150920925+fdemirhisar@users.noreply.github.com>
2024-05-04 00:59:59 +0200

    Update formsubmission.js

--- formsubmission.js
+++ formsubmission.js
@@ -3,14 +3,12 @@ function submitForm(event) {
 
   const username = document.querySelector('input[name="username"]').value;
   const password = document.querySelector('input[name="password"]').value;
-
-  // Simülasyon amaçlı olarak başarılı login olduğunu varsayalım
-  // Gerçek uygulamada sunucu tarafında kontrol yapılmalı
+  
   if (username === 'example' && password === 'password') {
     // Başarılı login durumunda yönlendirme
     window.location.href = "login_granted.html";
   } else {
-    // Başarısız giriş durumunda uyarı verebiliriz
+
     alert("Invalid username or password");
   }
 }
