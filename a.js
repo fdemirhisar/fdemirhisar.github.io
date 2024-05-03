910347eb3d34439c2d6ba45736c2e25e9fdc89b2
Filiz <150920925+fdemirhisar@users.noreply.github.com>
2024-05-03 11:10:48 +0200

    Update a.js

--- a.js
+++ a.js
@@ -1,11 +1,23 @@
+// script.js
 
+function checkInput() {
+  const username = document.querySelector('input[name="username"]').value;
+  const password = document.querySelector('input[name="password"]').value;
+  const loginButton = document.querySelector('input[name="login"]');
+  
+  if (username.trim() !== '' && password.trim() !== '') {
+    loginButton.disabled = false;
+    loginButton.classList.add('active'); // 'active' class'ını ekle
+  } else {
+    loginButton.disabled = true;
+    loginButton.classList.remove('active'); // 'active' class'ını kaldır
+  }
+}
 
-const loginForm = document.getElementById('loginForm');
-const message = document.getElementById('message');
-
-loginForm.addEventListener('submit', async (event) => {
+function submitForm(event) {
   event.preventDefault();
 
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
+  const username = document.querySelector('input[name="username"]').value;
+  const password = document.querySelector('input[name="password"]').value;
 
+}
